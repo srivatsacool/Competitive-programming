cde5829d3bcf8dba7b808ff7be63b1097d3843dd
srivatsacool <76219802+srivatsacool@users.noreply.github.com>
2021-09-16 01:57:57 +0530

    Update 242. Valid Anagram.cpp

--- Leetcode/Sorting/242. Valid Anagram.cpp
+++ Leetcode/Sorting/242. Valid Anagram.cpp
@@ -3,7 +3,7 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        
+        if(s.length()!=t.length())return false;
         for(int i = s.length() ; i >= 0 ; i--)
         {
             if(s[i] != t[i])return false;
