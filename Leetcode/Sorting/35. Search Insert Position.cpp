22f2a3cb4e425af7cfe427c8cb6cbd0c55a6759a
srivatsacool <76219802+srivatsacool@users.noreply.github.com>
2021-09-19 01:34:27 +0530

    Update 35. Search Insert Position.cpp

--- Leetcode/Sorting/35. Search Insert Position.cpp
+++ Leetcode/Sorting/35. Search Insert Position.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        if(target < nums.front())return 0;
+        if(nums.back() < target)return nums.size();
         for(int i = 0 ; i < nums.size() ;i++ ){
             if(nums[i]==target)return i;
-            else if(nums[i] == nums.front() && target < nums[i])return i;
             
-            else if(nums[i]<target && nums[i]==nums.back())return i+1;
-            
-            else if(nums[i]!=nums.back() && nums[i+1]>target )return i+1;
-
+            else if(nums[i+1]>target )return i+1;
             }
     return 0 ;    
     }
