e49e11c0d0b572b803f1881807f64d10129096ca
srivatsacool <76219802+srivatsacool@users.noreply.github.com>
2021-09-19 03:02:21 +0530

    Update 905. Sort Array By Parity.cpp

--- Leetcode/Sorting/905. Sort Array By Parity.cpp
+++ Leetcode/Sorting/905. Sort Array By Parity.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    bool isEven(int& x) {
+        return x % 2 == 0;
+    }
+    vector<int> sortArrayByParity(vector<int>& A) {
+        deque <int> fi;
+        for(int i = 0 ; i < A.size() ; i++){
+            if(A[i]%2==0)fi.push_front(A[i]);
+            else fi.push_back(A[i]);
+        }
+        return vector<int> (fi.begin(),fi.end());
+    }
+};
+
+
+
+
+
+
+class Solution {
+public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         vector<int> fi ;
         vector<int> odd ;
