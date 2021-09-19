7da4133a2b97d2389d1043366296252323141589
srivatsacool <76219802+srivatsacool@users.noreply.github.com>
2021-09-20 02:18:50 +0530

    Create 976. Largest Perimeter Triangle.cpp

---
+++ Leetcode/Sorting/976. Largest Perimeter Triangle.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        if(nums.size()<3)return 0;
+        sort(nums.begin(),nums.end(),greater<int>());
+        for(int i = 0 ; i < nums.size()-2 ; i++){
+            if(nums[i] < nums[i+1]+nums[i+2])return nums[i]+nums[i+1]+nums[i+2];
+        }
+    return 0 ;
+    }
+};
