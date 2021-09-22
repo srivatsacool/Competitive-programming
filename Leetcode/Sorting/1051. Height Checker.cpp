bee267943de14241e432fcd406bc686e2f63e595
srivatsacool <76219802+srivatsacool@users.noreply.github.com>
2021-09-23 00:51:02 +0530

    Create 1051. Height Checker.cpp

---
+++ Leetcode/Sorting/1051. Height Checker.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count = 0 ;
+        vector<int>temp (heights.begin(),heights.end());
+        sort(temp.begin(),temp.end());
+        for(int i = 0 ; i < temp.size();i++){
+            if(heights[i] != temp[i])count++;
+        }
+    return count ;
+    }
+};
